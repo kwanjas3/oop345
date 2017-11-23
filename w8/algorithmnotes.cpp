4663d36ac48174aac200ca1ec5337492a4cfcd75
kwanjas3 <kwanjas3@users.noreply.github.com>
2017-11-23 10:57:06 -0500

    lol nice

---
+++ w8/algorithmnotes.cpp
@@ -0,0 +1,24 @@
+//algorithm numeric functional notes.cpp
+
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+
+/*
+Milestones for the Project, 
+
+graph foo {
+1x -- A99
+}
+
+digraph foo {
+1x->A99
+}
+
+*/
+int main (int argc, char* argv []){
+
+
+
+}
